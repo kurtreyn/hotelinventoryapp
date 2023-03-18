2d1f02b0d4cc0f3620c29d277a3133ae2f613220
kurtreyn <kurtreyn@yahoo.com>
2023-03-18 12:55:03 -0400

    binding syntax branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
   // template: `<h1>Hello world from inline template</h1>
   //   <p>Angular is awesome!</p> `,
